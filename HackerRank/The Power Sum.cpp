ce9249b5ab1b9def0c422d5a8dc65ec115dfcee7
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-02-27 12:58:26 +0530

    Create The Power Sum.cpp

---
+++ HackerRank/The Power Sum.cpp
@@ -0,0 +1,21 @@
+/*
+APPROACH: BACKTRACKING
+
+COMPLEXITY:
+    - TC = O(2^(pow(X, 1.0/N)))
+    - SC = O(1)
+*/
+int dfs(int i, int lim, int X, int N) {
+    if(X == 0) return 1;
+    if(i>lim || X<0) return 0;
+    
+    int include = dfs(i+1, lim, X-pow(i,N), N);
+    int exclude = dfs(i+1, lim, X, N);
+    
+    return include + exclude;
+}
+
+int powerSum(int X, int N) {
+    int lim = pow(X, 1.0/N);
+    return dfs(1, lim, X, N);
+}
